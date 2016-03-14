b0731113b5de8deed6e4f668e5535f9030a56743
Serii <dharm.serg@gmail.com>
2016-03-14 23:17:05 +0200

    (wip) Adding routing, stopped at "Dashboard Top Heroes" part

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,39 @@
 import { Component } from 'angular2/core';
-import { HeroService } from './hero.service';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
 	selector: 'my-app',
 	template: `
 		<h1>{{title}}</h1>
-		<my-heroes></my-heroes>
+		<nav>
+			<a [routerLink]="['Dashboard']">Dashboard</a>
+			<a [routerLink]="['Heroes']">Heroes</a>
+		</nav>
+		<router-outlet></router-outlet>
 	`,
-	directives: [HeroesComponent],
+	directives: [ROUTER_DIRECTIVES],
 	providers: [
+		ROUTER_PROVIDERS,
 		HeroService
 	]
 })
-
+@RouteConfig([
+	{
+		path: '/heroes',
+		name: 'Heroes',
+		component: HeroesComponent
+	},
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: DashboardComponent,
+		useAsDefault: true
+	}
+])
 export class AppComponent {
 	title = 'Tour of Heroes';
 }
