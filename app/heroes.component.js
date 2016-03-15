6a74013447453697f528d3a8242f24a1db68e14f
banzayche <dharm.serg@gmail.com>
2016-03-15 12:33:39 +0200

    5. Added routing

--- app/heroes.component.js
+++ app/heroes.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero-detail.component', './hero.service'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', './hero-detail.component', './hero.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,15 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_detail_component_1, hero_service_1;
+    var core_1, router_1, hero_detail_component_1, hero_service_1;
     var HeroesComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             },
             function (hero_detail_component_1_1) {
                 hero_detail_component_1 = hero_detail_component_1_1;
@@ -27,30 +30,34 @@ System.register(['angular2/core', './hero-detail.component', './hero.service'], 
             // This is metadata for class
             HeroesComponent = (function () {
                 // to add new _heroService property and identifies it as a HeroService injection
-                function HeroesComponent(_heroService) {
+                function HeroesComponent(_router, _heroService) {
+                    this._router = _router;
                     this._heroService = _heroService;
                     this.title = 'Tour of Heroes';
                 }
                 HeroesComponent.prototype.getHeroes = function () {
                     var _this = this;
-                    // this._heroService.getHeroes().then(heroes => this.heroes = heroes);
-                    // or we can use slow mode:)
                     this._heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+                    // or we can use slow mode:)
+                    // this._heroService.getHeroes().then(heroes => this.heroes = heroes);
                 };
                 // do something when initialize
                 HeroesComponent.prototype.ngOnInit = function () {
                     this.getHeroes();
                 };
                 HeroesComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+                HeroesComponent.prototype.gotoDetail = function () {
+                    this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+                };
                 HeroesComponent = __decorate([
                     core_1.Component({
                         selector: 'my-heroes',
-                        template: "\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"#hero of heroes\"\n        [class.selected]=\"hero === selectedHero\"\n        (click)=\"onSelect(hero)\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n    <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  ",
-                        styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .heroes {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 10em;\n    }\n    .heroes li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .heroes li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .heroes li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .heroes .text {\n      position: relative;\n      top: -3px;\n    }\n    .heroes .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "],
+                        templateUrl: 'app/templates/heroes.component.html',
+                        styleUrls: ['app/templates/heroes.component.css'],
                         //for another components to add into DOM
                         directives: [hero_detail_component_1.HeroDetailComponent],
                     }), 
-                    __metadata('design:paramtypes', [hero_service_1.HeroService])
+                    __metadata('design:paramtypes', [router_1.Router, hero_service_1.HeroService])
                 ], HeroesComponent);
                 return HeroesComponent;
             }());
