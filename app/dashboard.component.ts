c7412fa33ea68c33e0be275d460b8e2d3c5b9a15
banzayche <dharm.serg@gmail.com>
2016-03-15 12:02:52 +0200

    added navigation from dashboard to hero/detail/:id

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,30 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { Router } from 'angular2/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'my-dashboard',
-	template: '<h3>My Dashboard</h3>'
+	templateUrl: 'app/templates/dashboard.component.html',
 })
-export class DashboardComponent { }
+export class DashboardComponent { 
+	heroes: Hero[] = [];
+	
+	constructor(
+		private _router: Router,
+		private _heroService: HeroService) {
+	}
+
+	ngOnInit() {
+		this._heroService.getHeroes()
+			.then(heroes => this.heroes = heroes.slice(1, 5));
+	}
+
+	// set a route link parameters array
+	// pass the array to the router's navigate method.
+	gotoDetail(hero: Hero) {
+		let link = ['HeroDetail', { id: hero.id }];
+		this._router.navigate(link);
+	}
+}
